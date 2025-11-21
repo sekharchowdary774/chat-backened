098c17fb44fdd2190d4ac5a81f7cb2c25e627799
sekhar <sekharkamma5@gmail.com>
2025-11-21 15:42:26 +0530

    add maven wrapper2

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# install java 17
+export JAVA_VERSION=17
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt-get update
+apt-get install -y openjdk-17-jdk
+
+# verify java
+java -version
+
+# give mvnw permission
 chmod +x mvnw
+
+# build project
 ./mvnw clean package -DskipTests
